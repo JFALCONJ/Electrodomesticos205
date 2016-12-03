12d9f88ec04e54a8539ecf7e50d1620f7948e076
BRYAN BUITRAGO <BRYAN BUITRAGO@10.1.23.69>
2016-12-03 15:10:13 -0500

    Creacion Clase PS5

---
+++ src/calle13/PS5.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calle13;
+
+/**
+ *
+ * @author BRYAN BUITRAGO
+ */
+public class PS5 extends Electrodomestico{
+
+    public PS5(String marca, String entradaelectrica) {
+        super(marca, entradaelectrica);
+    }
+
+    @Override
+    void enceder() {
+        
+    }
+    
+    
+}

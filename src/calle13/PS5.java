06c17d596898409bc6b1335b07e6d65c4ce95710
BRYAN BUITRAGO <BRYAN BUITRAGO@10.1.23.69>
2016-12-03 15:48:57 -0500

    Creacion Clase PS5

--- src/calle13/PS5.java
+++ src/calle13/PS5.java
@@ -10,15 +10,97 @@ package calle13;
  * @author BRYAN BUITRAGO
  */
 public class PS5 extends Electrodomestico{
+    
+    double alto, ancho;
+    String color;
+    String leerjuego;
+    boolean encender;
+    boolean abrirbandeja;
+    boolean subir, bajar, izquierda, derecha;
+    boolean botonX, botonCuadrado, botonTriangulo, botonCirculo;
+    boolean start, selec;
+    boolean prenderConsola;
+    boolean botonR1,botonR2,botonL1,botonL2;
 
-    public PS5(String marca, String entradaelectrica) {
+    public PS5(double alto, double ancho, String marca, String entradaelectrica) {
         super(marca, entradaelectrica);
+        this.alto = alto;
+        this.ancho = ancho;
     }
-
+    
+    public void encender()
+    {
+        
+    }
+    public void abrirBandeja()
+    {
+        
+    }
+    public void seleccionJuego()
+    {
+        
+    }
+    public void saltar()
+    {
+        
+    }
+   
+    public void agacharse()
+    {
+        
+    }
+    public void derecha()
+    {
+        
+    }
+    public void izquierda()
+    {
+        
+    }
+    public void patadaAlta()
+    {
+        
+    }
+    public void patadaBaja()
+    {
+        
+    }
+    public void puñoAlto()
+    {
+        
+    }
+    public void puñoBajo()
+    {
+        
+    }
+    public void pausa()
+    {
+        
+    }
+    public void selector()
+    {
+        
+    }
+    public void combo1()
+    {
+        
+    }
+    public void combo2()
+    {
+        
+    }
+    public void combo3()
+    {
+        
+    }
+    public void combo4()
+    {
+        
+    }
     @Override
     void enceder() {
         
     }
-    
+        
     
 }
